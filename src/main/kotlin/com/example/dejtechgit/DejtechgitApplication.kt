3a157b20a054777e2213e964091b407f530fdd7a
mbarroux <mickael.barroux@gmail.com>
2018-06-07 15:42:51 +0200

    Init projet Dejtech GIT

---
+++ src/main/kotlin/com/example/dejtechgit/DejtechgitApplication.kt
@@ -0,0 +1,11 @@
+package com.example.dejtechgit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DejtechgitApplication
+
+fun main(args: Array<String>) {
+    runApplication<DejtechgitApplication>(*args)
+}
